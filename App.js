5604bdeb45f4e6d4bd05d45656ae3f10c113a6e4
Sean Murakami <murakami.sean@yahoo.com>
2018-11-29 00:16:32 -0800

    updated App.js to include SafeAreaView

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -8,9 +8,11 @@ export default class App extends React.Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={{fontSize: 50}}>Hello, World</Text>
-      </View>
+      <SafeAreaView style={{flex: 1, backgroundColor: '#eee'}}>
+        <View style={styles.container}>
+          <Text style={{fontSize: 50}}>Hello, World</Text>
+        </View>
+      </SafeAreaView>
     );
   }
 }
@@ -19,8 +21,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#CFCFCF',
-    padding: 10,
-    paddingTop: 40
+    padding: 10
   },
 });
