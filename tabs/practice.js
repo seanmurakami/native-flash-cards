88fe69d9a41566be06c53f1fe7c6f48f3eb6ae16
Sean Murakami <murakami.sean@yahoo.com>
2018-12-09 22:38:25 -0800

    #6 updated the practice card component to use FlatList instead of ScrollView to increase the render speed && updated each card to take up the full width of the screen

--- tabs/practice.js
+++ tabs/practice.js
@@ -1,44 +1,48 @@
 import React from 'react'
-import { StyleSheet, ScrollView, Text, View } from 'react-native'
+import { StyleSheet, FlatList, Text, View, Dimensions } from 'react-native'
 
 export default class PracticeCard extends React.Component {
   render() {
     return (
-      <View style={ styles.main }>
-        <View style={ styles.container }>
-          <ScrollView horizontal pagingEnabled showsHorizontalScrollIndicator={false}>
-            {
-              this.props.screenProps.flashcards.map((item, i) => {
-              return (
-              <View key={i} style={ styles.card }>
-                <Text style={ styles.values }>{item.question}</Text>
+      <View style={ styles.container }>
+        <View style={ styles.main }>
+          <FlatList
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            data={this.props.screenProps.flashcards}
+            keyExtractor={item => item.id.toString()}
+            renderItem={({item}) =>
+              <View style={ styles.currentCard }>
+                <View style={ styles.card }>
+                  <Text style={ styles.values }>{item.question}</Text>
+                </View>
               </View>
-              )
-            })
-            }
-          </ScrollView>
-        </View>
+              }/>
+          </View>
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  main: {
+  container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
   },
-  container: {
+  main: {
     width: '100%'
+  },
+  currentCard: {
+    width: Dimensions.get('window').width
   },
   card: {
     alignItems: 'center',
     justifyContent: 'center',
-    margin: 10,
     paddingHorizontal: 10,
     height: 200,
-    width: 355,
+    margin: 9,
     borderRadius: 9,
     backgroundColor: '#eee',
     shadowOffset: {width: 1, height: 1},
